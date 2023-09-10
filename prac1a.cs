b1a43f42ee145438a44482897a59ba905b954beb
mahesh70588 <138879761+mahesh70588@users.noreply.github.com>
2023-09-10 18:54:38 +0530

    Add files via upload

---
+++ prac1a.cs
@@ -0,0 +1,22 @@
+using System;
+namespace prac1a{
+class prac1a{
+public static void Main(){
+Console.WriteLine("starting of first program");
+int num1,num2,num3,ans;
+Console.WriteLine("enter first number");
+num1=int.Parse(Console.ReadLine());
+Console.WriteLine("enter second number");
+num2=int.Parse(Console.ReadLine());
+Console.WriteLine("enter third number");
+num3=int.Parse(Console.ReadLine());
+ans=num1*num2*num3;
+Console.WriteLine("product is "+ans);
+Console.ReadKey();
+
+
+
+
+}
+}
+}
